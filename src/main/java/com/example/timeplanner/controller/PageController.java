aacfe63c8e94718d9b2148b03ea277e8fcd7f516
Jagadeesh Karuturi <kjagadeesh620@gmail.com>
2026-02-18 22:08:33 +0530

    done with browse , about and 5 recipe controller and ui

--- src/main/java/com/example/timeplanner/controller/PageController.java
+++ src/main/java/com/example/timeplanner/controller/PageController.java
@@ -21,4 +21,33 @@ public class PageController {
         return "about";
     }
 
+    @GetMapping("/recipe/chicken-biryani")
+    public String chickenBiryani() {
+        return "fooditems/chicken-biryani";
+    }
+
+    @GetMapping("/recipe/paneer-butter-masala")
+    public String paneerButterMasala() {
+        return "fooditems/paneer-butter-masala";
+    }
+
+    @GetMapping("/recipe/masala-dosa")
+    public String masalaDosa() {
+        return "fooditems/masala-dosa";
+    }
+
+    @GetMapping("/recipe/gulab-jamun")
+    public String gulabJamun() {
+        return "fooditems/gulab-jamun";
+    }
+
+    @GetMapping("/recipe/veg-fried-rice")
+    public String vegFriedRice() {
+        return "fooditems/veg-fried-rice";
+    }
+
+    @GetMapping("/recipe/butter-chicken")
+    public String butterChicken() {
+        return "fooditems/butter-chicken";
+    }
 }
